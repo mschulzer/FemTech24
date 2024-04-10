65ec0479c7bc2edd7e68f2217e834aabdf4859f0
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2024-04-10 11:55:35 +0200

    Create send-api.py

---
+++ send-api.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+url = "https://ml.dataekspeditioner.dk/add/"
+
+payload = json.dumps({
+  "email": "peaches@marioland.com",
+  "state": "#fffccc"
+})
+
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response) # OK <200>
