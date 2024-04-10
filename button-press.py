a7a58618d132132e53bf9c173d6b0555a504b6f6
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2024-04-10 13:15:34 +0200

    Create button-press.py

---
+++ button-press.py
@@ -0,0 +1,10 @@
+from machine import Pin
+import utime
+ 
+button = Pin(16, Pin.IN, Pin.PULL_UP)
+ 
+while True:
+    b1 = button.value()
+    if not b1:
+        print('Button pressed!')
+        utime.sleep(0.5)
